eb4be33f4509eb8b3d920b26fea5d3857a0785cd
raianeeunice <liraradasa@gmail.com>
2021-12-08 11:24:10 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CalculadoraModule } from './calculadora';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalculadoraModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calculadora/services/calculadora.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculadoraService {
+
+  /* Define as constantes utilizadas para identificar as operações de cálculo. Ps: é bom criar const com
+  letras maiusculas */
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly MULTIPLICACAO: string ='*';
+  static readonly DIVISAO: string ='/';
+
+  constructor() {}
+
+  // metodo que calcula uma operação matemática dado dois números
+  calcular(num1: number, num2: number, operacao:string): number {
+    let resultado: number; // armazena o resultado da operação
+    
+    switch(operacao){
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+      break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+      break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+      break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+      break;
+      default:
+        resultado =0;
+    }
+    return resultado;
+  }
+
+}
